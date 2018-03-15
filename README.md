2f50c9f961ab13c6908db2bff4f0b873f56a9083
root <root@localhost.localdomain>
2018-03-15 12:37:44 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gfw2pac
+
+## 介绍
+
+基于node环境实现[gfwlist](https://github.com/gfwlist/gfwlist)到pac file的转换工具，自动获取最新的gfwlist版本并且生成pac文件。
+
+使用`node gfw2pac.js -h获取更多帮助`
---
+++ gfw2pac.js
@@ -0,0 +1,58 @@
+const https = require('https');
+const fs = require('fs');
+
+const commander = require('commander');
+const promisfy = require('promisfy');
+
+const connect = promisfy.promisfyNoError(https.get, https);
+const readfile = promisfy.promisfy(fs.readFile);
+const writefile = promisfy.promisfy(fs.writeFile);
+
+function base64ToString(str) {
+    return Buffer.from(str, 'base64').toString();
+}
+
+function processGfwRawData(gfwraw) {
+    let list = base64ToString(gfwraw).split('\n').filter((val , i) => {
+        if (val.startsWith('[') || val.startsWith('!') || val.length === 0) {
+            return false;
+        }
+
+        return true;
+    });
+
+    return list;
+}
+
+async function main(proxy, useTiny) {
+    let gfwurl;
+
+    if (useTiny) {
+        gfwurl = 'https://raw.githubusercontent.com/gfwlist/tinylist/master/tinylist.txt';
+    } else {
+        gfwurl = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+    }
+
+    try {
+        let conn = await connect(gfwurl);
+        let gfwraw = await promisfy.waitFor(conn, 'data');
+        let gfwlist = processGfwRawData(gfwraw);
+
+        let tpl = await readfile('./tpl.pac', {encoding: 'utf8'});
+
+        let result = tpl.replace('__RULES__', JSON.stringify(gfwlist))
+            .replace('__PROXY__', proxy ? proxy : '\'SOCKS5 127.0.0.1:1080\'');
+
+        await writefile('./gfw.pac', result);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+commander.version('1.0.0')
+.usage('[options]')
+.option('--proxy [proxy string]', 'a pac-file-liked proxy string, default to SOCK5 127.0.0.1:1080')
+.option('--tiny', 'use a tiny version of gfwlist, this may help reduce memory use of pac')
+.parse(process.argv);
+
+main(commander.proxy, commander.tiny);
